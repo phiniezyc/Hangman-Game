15ab80dd33e9ae96f37de9f86a362c76feced9d7
Chance Phiniezy <phiniezyc@gmail.com>
2017-08-25 06:13:42 -0400

    initial functionality for category coming up before user guesses

---
+++ assets/javascript/game.js
@@ -0,0 +1,51 @@
+//Game Mechanics:
+
+/*
+3. Use key events to listen for the letters that your players will type.
+
+4. Display the following on the page:
+
+    5. Press any key to get started!
+
+    6. Wins: (# of times user guessed the word correctly).
+
+* If the word is `madonna`, display it like this when the game starts: `_ _ _ _ _ _ _`.
+
+* As the user guesses the correct letters, reveal them: `m a d o _  _ a`.
+
+7. Number of Guesses Remaining: (# of guesses remaining for the user).
+
+8. Letters Already Guessed: (Letters the user has guessed, displayed like `L Z Y H`).
+
+9. After the user wins/loses the game should automatically choose another word and make the user play it. */
+
+
+//Onkey triggers game to start: letter comes up w/ ____ etc
+// Onkey checks to see if letter guessed
+// If correct then reveals that letter
+// If incorrect then puts it in array to be displayed as incorrect
+
+
+/* We take our Names put in Array */
+var atlantaSportsNames = ["Jones", "Nixon", "Freeman", "Vick", "Bream", "Wilkins", "Tuggle", "Glavine", "Smoltz", "Lopez"]
+
+var possibleGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+/* We take our Array and set it to index position Math.floor/random to get random number--this will give us a random position within the array. We then set it to a variable to use later.*/
+var nameToGuess = atlantaSportsNames[Math.floor(Math.random() * atlantaSportsNames.length)];
+
+console.log(nameToGuess);
+
+
+
+var numberOfCharacters = ""
+
+for (i = 0; i < nameToGuess.length; i++) {
+    numberOfCharacters += " _ "
+}
+
+document.getElementById("WhereGamePutsClue").innerHTML = numberOfCharacters;
+
+
+
+
+/*Let's see if we can get user guess by using Variable nameToGuess and */
